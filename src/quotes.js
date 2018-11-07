31af105ba32fa860a61b44dc219c92bb4ae3ccc4
Sergei Alimov <8912652@gmail.com>
2018-11-07 14:28:52 +0300

    add more quotes

--- src/quotes.js
+++ src/quotes.js
@@ -2,7 +2,35 @@ const quotes = [
   {
     quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
     author: 'Chinese Proverb'
-  }
+  },
+  {
+    quote: 'I attribute my success to this: I never gave or took any excuse.',
+    author: 'Florence Nightingale'
+  },
+  {
+    quote: 'Whatever the mind of man can conceive and believe, it can achieve.',
+    author: 'Napoleon Hill'
+  },
+  {
+    quote: 'You take your life in your own hands, and what happens? A terrible thing, no one to blame.',
+    author: 'Erica Jong'
+  },
+  {
+    quote: 'The question isn’t who is going to let me; it’s who is going to stop me.',
+    author: 'Ayn Rand'
+  },
+  {
+    quote: 'It is never too late to be what you might have been',
+    author: 'George Eliot'
+  },
+  {
+    quote: 'The person who says it cannot be done should not interrupt the person who is doing it.',
+    author: 'Chinese Proverb'
+  },
+  {
+    quote: 'How wonderful it is that nobody need wait a single moment before starting to improve the world.',
+    author: 'Anne Frank'
+  },
 ]
 
 export default quotes;
