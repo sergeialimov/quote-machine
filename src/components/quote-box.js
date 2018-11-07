e46a87f8629fa0f9c5cd741fd3f1d3a364f7b464
Sergei Alimov <8912652@gmail.com>
2018-11-07 14:15:06 +0300

    add randomly selcting quote

--- src/components/quote-box.js
+++ src/components/quote-box.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import './../styles/quote-box.css'
 
 class QuoteBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getRandom = this.getRandom.bind(this)
+  }
+  
+  getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
   
   render() {
-    const quote = 'Challenges are what make life interesting and overcoming them is what makes life meaningful.'
-    const author = 'Chinese Proverb';
+    const quotes = [
+      {
+        quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
+        author: 'Chinese Proverb'
+      }
+    ]
+    const num = this.getRandom(quotes.length);
     return (
       <div className="quote-box">
         <header className="quote">
-          <p>{quote}</p>
+          <p>{quotes[num].quote}</p>
         </header>
         <div className="author">
-          <p>{author}</p>
+          <p>- {quotes[num].author}</p>
         </div>
+        <button className="new-quote-btn">New quote</button>
       </div>
     );
   }
