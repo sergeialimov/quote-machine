ecdbe681d5aea3be28055821de829919fbef38e2
Sergei Alimov <8912652@gmail.com>
2018-11-08 19:12:20 +0300

    add background colors

--- src/quotes.js
+++ src/quotes.js
@@ -1,35 +1,43 @@
 const quotes = [
   {
     quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
-    author: 'Chinese Proverb'
+    author: 'Chinese Proverb',
+    color: '#77B1A9',
   },
   {
     quote: 'I attribute my success to this: I never gave or took any excuse.',
-    author: 'Florence Nightingale'
+    author: 'Florence Nightingale',
+    color: '#482E32',
   },
   {
     quote: 'Whatever the mind of man can conceive and believe, it can achieve.',
-    author: 'Napoleon Hill'
+    author: 'Napoleon Hill',
+    color: '#E64C3C',
   },
   {
     quote: 'You take your life in your own hands, and what happens? A terrible thing, no one to blame.',
-    author: 'Erica Jong'
+    author: 'Erica Jong',
+    color: '#15A085',
   },
   {
     quote: 'The question isn’t who is going to let me; it’s who is going to stop me.',
-    author: 'Ayn Rand'
+    author: 'Ayn Rand',
+    color: '#74A857',
   },
   {
     quote: 'It is never too late to be what you might have been',
-    author: 'George Eliot'
+    author: 'George Eliot',
+    color: '#FB6964',
   },
   {
     quote: 'The person who says it cannot be done should not interrupt the person who is doing it.',
-    author: 'Chinese Proverb'
+    author: 'Chinese Proverb',
+    color: '#27AE60',
   },
   {
     quote: 'How wonderful it is that nobody need wait a single moment before starting to improve the world.',
-    author: 'Anne Frank'
+    author: 'Anne Frank',
+    color: '#332224',
   },
 ]
 
