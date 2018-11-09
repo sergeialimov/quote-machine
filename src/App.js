3ed6811cdafcdf34f8548bd1946e3dc3ba7b84ab
Sergei Alimov <8912652@gmail.com>
2018-11-09 18:27:02 +0300

    move state logic to redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import QuoteBox from './components/quote-box.js';
 import quotes from './quotes.js'
+import { connect, Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const NUM = 'NUM';
+
+const updateNum = (num) => {
+  return {
+    type: NUM,
+    num
+  }
+};
+
+const numReducer = (state = 0, action) => {
+  switch (action.type) {
+    case NUM:
+      return [
+        action.num
+      ];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(numReducer);
 
 class App extends Component {
   constructor(props) {
