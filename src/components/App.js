8fcc743bc99a107d9b09fa2e73f4fbdcc06da10a
Sergei Alimov <8912652@gmail.com>
2018-11-13 19:21:14 +0300

    add tests for freecodecamp

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ import QuoteBox from './../containers/quoteBox.jsx';
 import quotes from './../quotes.js'
 
 class App extends Component {
+  // autotests for FreeCodeCamp
+  componentDidMount () {
+    const script = document.createElement("script");
+    script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
   render() {
     const style = {
       "backgroundColor": quotes[this.props.num].color,
